560ba2891a305de1882f6f489bbf1bcae4e4863c
sahithyanuvvula25 <sahithyanuvvula25@gmail.com>
2025-09-06 14:58:25 +0530

    learned operators

---
+++ operators.js
@@ -0,0 +1,62 @@
+//arithematic opeartors
+let x=10,y=3;
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x%y)
+console.log(x**y)
+//assignment operators
+let z=5;
+z+=3;
+console.log(z)
+z-=3;
+console.log(z)
+z*=3;
+console.log(z)
+z/=3
+console.log(z)
+z**=3
+console.log(z)
+z%=3
+console.log(z)
+//comparison operators
+let a=10,b="sahithya",c=10
+console.log(a==b)
+console.log(a!=b)
+console.log(a===c)
+console.log(a!==b)
+console.log(a<b)
+console.log(a>b)
+console.log(a>=c)
+console.log(a<=b)
+//logical operators
+let age=22
+console.log(age>20 && age<30)
+console.log(age>20 || age<30)
+console.log(!(age>18))
+//string operators
+let firstname="sahithya"
+let lastname="nuvvula"
+let fullname=firstname+" "+lastname
+console.log(fullname)
+let correctname=`hello ${firstname}${lastname} ,welcome to here`
+console.log(correctname)
+//ternary operators
+let myage=18;
+if(myage>18){
+    console.log("you can vote")
+}
+else{
+    console.log("you cannot vote")
+}
+let result=age>18 ? "eligible to vote" :" cannot vote"
+//increment and decrement operators
+let count=5
+console.log(count++)
+console.log(count)
+console.log(++count)
+console.log(count)
+console.log(typeof count)
+console.log(typeof "age")
+console.log(typeof 18)
