8bbcff2f9272303efc0b639aebfb0e3c455e7465
sahithyanuvvula25 <sahithyanuvvula25@gmail.com>
2025-09-06 15:58:16 +0530

    learned functions

---
+++ functions.js
@@ -0,0 +1,33 @@
+function greet()
+{
+    console.log("welocome to devastra")
+}
+greet();
+function add(x,y)
+{
+    return x+y
+}
+let result=add(5,4)
+console.log(result)
+const pi=3.14
+console.log(pi)
+const multiply=function(x,y)
+{
+    return x+y
+}
+console.log(multiply(2,3))
+//arrow function
+const square=(x)=>x*x;
+console.log(square(5))
+const subtract=(x,y)=>
+{
+    console.log(x,y)
+    return x-y
+}
+console.log(subtract(15,20))
+ division =function (x,y)
+{
+    return x/y
+}
+console.log(division(10,3))
+
